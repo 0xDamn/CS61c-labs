39773b5d508deb61dda7fa8e51389ef9ae1d82cd
0xDamn <zjw_pub@163.com>
2022-09-13 15:30:55 +0800

    Lab01 Fin

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include "ex1.h"
+#include <string.h>
 
 /* Returns the number of times LETTER appears in STR.
 There are two different ways to iterate through a string.
@@ -7,13 +7,24 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+    int count = 0;
+    while (*str) {
+        if (*str == letter) {
+            count++;
+        }
+        str++;
+    }
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
-Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
-All letters will be upper case. */
+Each sequence will end with a NULL terminator and will have up to 20
+nucleotides. All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    dna_seq->A_count = num_occurrences(dna_seq->sequence, 'A');
+    dna_seq->C_count = num_occurrences(dna_seq->sequence, 'C');
+    dna_seq->T_count = num_occurrences(dna_seq->sequence, 'T');
+    dna_seq->G_count = num_occurrences(dna_seq->sequence, 'G');
     return;
 }
--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,21 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL || head->next == NULL || head->next->next == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head->next;
+    node *hare = head->next->next;
+
+    while (tortoise != hare) {
+        if (hare->next == NULL || hare->next->next == NULL) {
+            return 0;
+        }
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+    }
+    return 1;
 }
